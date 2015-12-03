c0657d660558cb8a21315349f7f52156b15af73d
Danish Mujeeb <danish.mujeeb@gmail.com>
2015-12-02 22:08:15 -0500

    Added JSON response capabilities

--- web.py
+++ web.py
@@ -1,7 +1,10 @@
 import os
+import json
 
 import jinja2
 import webapp2
+
+from mdetect import UAgentInfo
 
 # Declaring the path of the templates
 TEMPLATES_PATH = os.path.join(os.path.dirname(__file__), 'templates')
@@ -14,14 +17,56 @@ JINJA_ENVIRONMENT = jinja2.Environment(
 
 
 class MainPage(webapp2.RequestHandler):
-    ''' A class to handle requests for the main page '''
+    """ A simple example of the mdetect capabilities. We call a few
+        'detect' properties manually and display the result as a
+        simple webpage
+    """
 
     def get(self):
-        data = {'greeting': 'Hello World!'}
+        user_agent = str(self.request.headers['User-Agent'])
+        http_accept = str(self.request.headers['Accept'])
+        ua_info = UAgentInfo(user_agent, http_accept)
+
+        capabilities = {}
+        capabilities['Is Smartphone'] = ua_info.detectSmartphone()
+        capabilities['Is iOS'] = ua_info.detectIos()
+        capabilities['Is IPad'] = ua_info.detectIpad()
+        capabilities['Is Android'] = ua_info.detectAndroid()
+
+        data = {}
+        data['capabilities'] = capabilities
+        data['user_agent'] = user_agent
+        data['http_accept'] = http_accept
+
         template = JINJA_ENVIRONMENT.get_template('index.html')
         self.response.write(template.render(data))
+
+
+class Detector(webapp2.RequestHandler):
+    """ A class representing a more complex example of the mdetect
+        capabilities. It uses dir to detect any property that
+        begins with 'detect' and calls it to read the value
+    """
+
+    def get(self):
+        user_agent = str(self.request.headers['User-Agent'])
+        http_accept = str(self.request.headers['Accept'])
+        ua_info = UAgentInfo(user_agent, http_accept)
+
+        capabilities = dict()
+
+        for obj in dir(ua_info):
+            attr = getattr(ua_info, obj)
+            if callable(attr) and obj.startswith('detect'):
+                capabilities[obj.replace('detect', '')] = attr()
+
+        print capabilities
+        self.response.headers['Content-Type'] = 'application/json' 
+        self.response.write(json.dumps(capabilities))
+
 
 # Creating the actual application object
 app = webapp2.WSGIApplication([
     ('/', MainPage),
+    ('/detect/', Detector)
 ], debug=True)
